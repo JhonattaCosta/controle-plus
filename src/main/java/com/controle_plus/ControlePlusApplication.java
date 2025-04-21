e645cde3fc51ae651b4bbb2d6caa07b1d9dfda19
JhonattaCosta <jhonattasantos27@gmail.com>
2025-04-21 20:01:41 -0300

    feat: add my projet

---
+++ src/main/java/com/controle_plus/ControlePlusApplication.java
@@ -0,0 +1,13 @@
+package com.controle_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controle_plus/ControlePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controle_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
