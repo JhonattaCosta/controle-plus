d7abf2ea7f1e698db32084b58bd465cb1f900b15
Jhonatta <jhonattasantos27@gmaol.com>
2025-04-22 22:35:08 -0300

    feat: add annotations in the Entity

--- src/main/java/com/controle_plus/model/SubCategory.java
+++ src/main/java/com/controle_plus/model/SubCategory.java
@@ -1,10 +1,30 @@
 package com.controle_plus.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubCategory {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name",unique = true)
+    @NonNull
     private String name;
+
+    @Column(name = "essential_type")
+    @NonNull
     private EssentialType essentialType;
+
+    @Column(name = "user_id")
     private Long userId;
 
 }
