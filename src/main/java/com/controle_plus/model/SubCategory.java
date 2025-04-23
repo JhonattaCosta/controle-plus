e57a2a68f73db9ae8b59e9006747f32bd5530e4b
Jhonatta <jhonattasantos27@gmaol.com>
2025-04-22 22:30:52 -0300

    feat: add entity SubCategoryModel

---
+++ src/main/java/com/controle_plus/model/SubCategory.java
@@ -0,0 +1,10 @@
+package com.controle_plus.model;
+
+public class SubCategory {
+
+    private Long id;
+    private String name;
+    private EssentialType essentialType;
+    private Long userId;
+
+}
