ccfa645bb6741900f2e950260b9128ae8ab97bd2
JhonattaCosta <jhonattasantos27@gmail.com>
2025-04-24 20:41:43 -0300

    fix: remove imports

--- src/main/java/com/controle_plus/repository/ExpenseRepository.java
+++ src/main/java/com/controle_plus/repository/ExpenseRepository.java
@@ -4,11 +4,8 @@ import com.controle_plus.model.EssentialType;
 import com.controle_plus.model.ExpensesModel;
 import com.controle_plus.model.SubCategoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository <ExpensesModel, Long> {
