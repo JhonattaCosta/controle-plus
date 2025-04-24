a7655b1eaa20bc206fa8efd8d7c1d0190f5fd5a0
JhonattaCosta <jhonattasantos27@gmail.com>
2025-04-24 12:54:39 -0300

    feat: add custom seach methods for SubCategoryRepository

--- src/main/java/com/controle_plus/repository/SubcategoryRepository.java
+++ src/main/java/com/controle_plus/repository/SubcategoryRepository.java
@@ -1,7 +1,16 @@
 package com.controle_plus.repository;
 
+import com.controle_plus.model.EssentialType;
 import com.controle_plus.model.SubCategoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface SubcategoryRepository extends JpaRepository <SubCategoryModel, Long> {
+
+    List<SubCategoryModel> findByNameContainingIgnoreCase(String name);
+
+    List<SubCategoryModel> findByEssentialType(EssentialType essentialType);
+
+    List<SubCategoryModel> findByNameContainingIgnoreCaseAndEssentialType(String name, EssentialType essentialType);
+
 }
