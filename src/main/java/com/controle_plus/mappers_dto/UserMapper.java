b6f933e60fb5811aa9b9369a2834e9ede694376a
JhonattaCosta <jhonattasantos27@gmail.com>
2025-04-24 18:11:37 -0300

    feat: add UserMapper

--- src/main/java/com/controle_plus/mappers_dto/UserMapper.java
+++ src/main/java/com/controle_plus/mappers_dto/UserMapper.java
@@ -1,4 +1,34 @@
 package com.controle_plus.mappers_dto;
 
+import com.controle_plus.model.UserModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public UserModel map(UserDTO userDTO) {
+        UserModel userModel = new UserModel();
+
+        userModel.setId(userDTO.getId());
+        userModel.setUsername(userDTO.getUsername());
+        userModel.setPassword(userDTO.getPassword());
+        userModel.setEmail(userDTO.getEmail());
+        userModel.setDateOfBirth(userDTO.getDateOfBirth());
+        userModel.setRole(userDTO.getRole());
+
+        return userModel;
+    }
+
+    // Mapeamento de UserModel para UserDTO
+    public UserDTO map(UserModel userModel) {
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setId(userModel.getId());
+        userDTO.setUsername(userModel.getUsername());
+        userDTO.setPassword(userModel.getPassword());
+        userDTO.setEmail(userModel.getEmail());
+        userDTO.setDateOfBirth(userModel.getDateOfBirth());
+        userDTO.setRole(userModel.getRole());
+
+        return userDTO;
+    }
 }
